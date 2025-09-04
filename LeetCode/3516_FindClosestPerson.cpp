8c9203025ab8d0afcb06b90280520cecbf518f23
Radha-03 <132573443+Radha-03@users.noreply.github.com>
2025-09-04 11:21:06 +0530

    Create 3516_FindClosestPerson.cpp

---
+++ LeetCode/3516_FindClosestPerson.cpp
@@ -0,0 +1,17 @@
+/*You are given three integers x, y, and z, representing the positions of three people on a number line:
+
+x is the position of Person 1.
+y is the position of Person 2.
+z is the position of Person 3, who does not move.
+Both Person 1 and Person 2 move toward Person 3 at the same speed.
+
+Determine which person reaches Person 3 first:**/
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int b=abs(z-y);
+        int a=abs(z-x);
+        return (a != b) << (a > b);
+    }
+};
