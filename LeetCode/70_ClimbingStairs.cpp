0b860f57aa8c51ddd710c5a5d59508f56ae4d4aa
Radha-03 <132573443+Radha-03@users.noreply.github.com>
2025-10-05 14:16:19 +0530

    Create 70_ClimbingStairs.cpp

---
+++ LeetCode/70_ClimbingStairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int count(int n, vector<int>& dp) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+        if (dp[n] != -1) return dp[n];  // already computed
+
+        dp[n] = count(n-1, dp) + count(n-2, dp);
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1); // initialize with -1
+        return count(n, dp);
+    }
+};
