f3b86e28f807f4ac4374534713f97b95328afadc
Radha-03 <132573443+Radha-03@users.noreply.github.com>
2025-12-26 21:29:34 +0530

    Implement palindrome check for linked list

---
+++ LeetCode/Link List/234. Palindrome Linked List.cpp
@@ -0,0 +1,17 @@
+// 234. Palindrome Linked List
+
+class Solution {
+    ListNode* curr;
+public:
+    bool isPalindrome(ListNode* head) {
+        curr = head;
+        return solve(head);
+    }
+
+    bool solve(ListNode* head) {
+        if (head == nullptr) return true;
+        bool ans = solve(head->next) && head->val == curr->val;
+        curr = curr->next;
+        return ans;
+    }
+};
