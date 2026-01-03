3b6ab0ebd60388da8bb1ec3d4be00ba53dbec6e9
Radha-03 <132573443+Radha-03@users.noreply.github.com>
2026-01-03 18:24:01 +0530

    Implement mergeTwoLists to merge two sorted lists

---
+++ LeetCode/Link List/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head=NULL;
+        ListNode *p=head;
+        while(list1!=NULL&&list2!=NULL){
+            if(list1->val<list2->val){
+                if(p==NULL){
+                    p=list1;
+                    head=list1;
+                    list1=list1->next;
+                    p->next=NULL;
+                }
+                else{
+                    p->next=list1;
+                    list1=list1->next;
+                    p=p->next;
+                    p->next=NULL;
+                }
+            }
+            else{
+                 if(p==NULL){
+                    p=list2;
+                    head=list2;
+                    list2=list2->next;
+                    p->next=NULL;
+                }
+                else{
+                    p->next=list2;
+                    list2=list2->next;
+                    p=p->next;
+                    p->next=NULL;
+                }
+            }
+        }
+        while(list1!=NULL){
+            if(p==NULL){
+                p=list1;
+                head=list1;
+                list1=list1->next;
+                p->next=NULL;
+            }
+            else{
+                p->next=list1;
+                list1=list1->next;
+                p=p->next;
+                p->next=NULL;
+            }
+        }
+        while(list2!=NULL){
+            if(p==NULL){
+                p=list2;
+                head=list2;
+                list2=list2->next;
+                p->next=NULL;
+            }
+            else{
+                p->next=list2;
+                list2=list2->next;
+                p=p->next;
+                p->next=NULL;
+            }
+        }
+        return head;
+    }
+};
